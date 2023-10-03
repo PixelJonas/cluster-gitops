16b557bd796da5093cb190d643f7baca3a46e86e
PixelJonas <5434875+PixelJonas@users.noreply.github.com>
2023-10-03 14:33:07 +0000

    fix sealed credentials for backblaze
    Signed-off-by: PixelJonas <5434875+PixelJonas@users.noreply.github.com>

--- kubeseal.sh
+++ kubeseal.sh
@@ -1,3 +1,3 @@
 #/bin/sh
 
-echo -n $1 | kubeseal --raw --controller-namespace sealed-secrets --namespace $3 --name $2
+echo -n "$1" | kubeseal --raw --controller-namespace sealed-secrets --namespace "$3" --name "$2"
