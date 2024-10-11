4b89c2d5736f41a7c1ce3183aac26bb92baa02d5
PixelJonas <5434875+PixelJonas@users.noreply.github.com>
2024-10-11 09:13:34 +0000

    clean up cluster-gitops
    Signed-off-by: PixelJonas <5434875+PixelJonas@users.noreply.github.com>

--- manifests/argocd/apps/sealed-secrets-operator/scripts/get-sealed-secrets-key.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Getting public key from Sealed Secrets secret and copying it to ~/.bitnami"
-echo "Create dir for Sealed Secrets public key. (~/.bitnami)."
-mkdir -p ~/.bitnami
-echo "Backup secret itself"
-kubectl get $(kubectl get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key=active -o name) -n sealed-secrets -o yaml >  ~/.bitnami/sealed-secrets-secret.yaml
-echo "Get the public key from the Sealed Secrets secret."
-kubectl get $(kubectl get secret -n sealed-secrets -l sealedsecrets.bitnami.com/sealed-secrets-key=active -o name) -n sealed-secrets -o jsonpath='{.data.tls\.crt}' | base64 --decode > ~/.bitnami/publickey.pem
